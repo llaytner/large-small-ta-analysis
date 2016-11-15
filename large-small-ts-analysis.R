fa2a91910fe7f7e0d9235e49b4d0ed2e139810ee
llaytner <llaytner@ufl.edu>
2016-11-15 15:28:09 -0500

    add function to load data

---
+++ large-small-ts-analysis.R
@@ -0,0 +1,6 @@
+# GIT #
+#steps to set up: 1) tell GIT who you are--> go to GIT tab in environ and click shell "git config -- global user.name "YOUR NAME""
+
+get_data <- function(){
+  read.csv("surveys.csv")
+}
