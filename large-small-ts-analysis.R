7bb87e6c17f70ee293ae7c31c27e43c4c29a3e5b
llaytner <llaytner@ufl.edu>
2016-11-15 15:34:58 -0500

    use a threshold for size class function

--- large-small-ts-analysis.R
+++ large-small-ts-analysis.R
@@ -4,3 +4,14 @@
 get_data <- function(){
   read.csv("surveys.csv")
 }
+#check box and check commit--diff space below the 3 new green lines getting added to this file
+#box with commit message, here you write what you did and why you did it and click commit
+#look at history, and you can see each thing you have done
+
+get_size_class <- function(weight) {
+  if(weight>50){ 
+    size_class = "large"
+  }else {size_class = "small"
+    return(size_class)
+  }
+} 
