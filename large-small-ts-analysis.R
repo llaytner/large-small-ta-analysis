5226acfeb3789b66fdfcfd2cd7cf74c42a67346e
llaytner <llaytner@ufl.edu>
2016-11-21 19:17:21 -0500

    new code that creates a data frame with information about the individual ID, the earth length class, and the gc-content for each individual. created data frame as a csv file using write.csv()

--- large-small-ts-analysis.R
+++ large-small-ts-analysis.R
@@ -28,6 +28,54 @@ main_data1 <- read.csv("houseelf_earlength_dna_data_1.csv")
 
 #5--adding a remote--
 
+#6 --pushing changes --
+#from assignment 7Now that you’ve set up your remote repository for collaborating with Dr. Granger you’d better get to work since she can see everything you’re doing.
+#Write a function to calculate the GC-content of a sequence, regardless of the capitalization of that sequence. 
+#(Hint: using the function str_to_lower or str_to_upper in the stringr package might be useful). 
+#This function should also be able to take a vector of sequences and return a vector of GC-contents (it probably does this without any extra work so give it a try).
+
+#Function to calculate the GC content
+library(stringr)
+
+get_GC_content=function(sequence){
+  sequence=str_to_lower(sequence)
+  Gs <- str_count(sequence, "g")
+  Cs <- str_count(sequence, "c")
+  gc_content <- (Gs + Cs) / str_length(sequence) * 100
+  return(gc_content)
+}
+#function that determines the earth length category
+get_ear_length <- function(seq){
+  ear_lengths <- ifelse(seq > 10, "large", "small")
+  return(ear_lengths)
+}
+
+#Read ID column from csv file
+ID <- data.frame(houseelf$id, stringsAsFactors = FALSE)
+dnaseq <- data.frame(houseelf$dnaseq, stringsAsFactors = FALSE)
+earlength <- data.frame(houseelf$seq, stringsAsFactors = FALSE)
 
 
+#calculate gc_content for all of the dna sequences
+gc_contents <- data.frame(gc_content = numeric(nrow(dnaseq)))
+for (i in 1:nrow(dnaseq)){
+  gc_contents[i,] <- get_gc_content(dnaseq[i,])
+}
+gc_contents
 
+#determine ear length category
+ear_length_cats <- data.frame(ear_lengths = numeric(nrow(seq())))
+for (i in 1:nrow(dnaseq)){
+  gc_contents[i,] <- get_gc_content(dnaseq[i,])
+}
+gc_contents
+
+# Read earlength column from csv file
+ear <- data.frame(earlengthcat = numeric(nrow(earlength)))
+for (i in 1:nrow(earlength)){
+  ear[i,] <- ear_length(earlength[i,])
+}
+ear
+
+houseelf_analysis <- data.frame(species= ID, ear, gc_contents, stringsAsFactors = FALSE)
+write.csv(houseelf_analysis.csv)
